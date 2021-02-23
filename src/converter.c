5e289b916d2376f70f0882d7042125edf191a88e
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2021-02-23 16:09:00 -0500

    cleaned up example code

--- src/converter.c
+++ src/converter.c
@@ -6,7 +6,7 @@ int main(){
 	float data = 2.25125;
 
 	// Convert float to unsigned integer
-	uint32_t float_to_uint = float_to_int(data);
+	uint32_t float_to_uint = float_to_uint32(data);
 	printf("float to uint32_t: %d\n", float_to_uint);
 	printf("uint32_t binary representation: ");
 	// Print uint32_t in binary (big-endian)
@@ -14,14 +14,20 @@ int main(){
 	printf("\n");
 
 	// Convert unsigned integer to float
-	float uint_to_float = int_to_float(float_to_uint);
+	float uint_to_float = uint32_to_float(float_to_uint);
 	printf("uint32_t to float: %.5f\n", uint_to_float);
 
-	// Convert integer into array of bytes (4 bytes for a uint32_t)
-	uint8_t* uint32_batch = uint32_t_batch(float_to_uint);
-	printf("uint32_t batch binary:\n");
-	print_integer32_batch(uint32_batch);
+	// Convert unsigned integer into array of bytes (4 bytes for a uint32_t)
+	uint8_t* uint32_to_batch = uint8_batch(float_to_uint);
+	printf("uint8_t_batch batch binary:\n");
+	print_binary8_batch(uint32_to_batch);
 	printf("\n");
-	free(uint32_batch);
-	
+
+	// Reconstruct unsigned integer back to float
+	uint32_t batch_to_uint = reconstruct_uint32(uint32_to_batch);
+	float batch_float = uint32_to_float(batch_to_uint);
+	printf("Reconstructed uint8_t batch to float: %.5f", batch_float);
+	printf("\n");
+
+	free(uint32_to_batch);
 }
